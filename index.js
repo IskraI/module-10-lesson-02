d0d5d602dd1279fd1e3d7f691ba522154b88f957
GoItTutor <sheva09a@gmail.com>
2023-01-26 23:15:28 +0200

    module-03-lesson-02

--- index.js
+++ index.js
@@ -1,138 +1,210 @@
 /*
-  1. Деструктуризація
-
-  Перепиши функцію так, щоб вона приймала один об'єкт параметрів
-  замість набору незалежних аргументів
+  1. Операція spread як заміна concat і slice
 */
 
-// function calcBMI(weight, height) {
-//   const numericWeight = Number(weight.replace(',', '.'));
-//   const numericHeight = Number(height.replace(',', '.'));
+//*  Копія массива
 
-//   return Number((numericWeight / numericHeight ** 2).toFixed(1));
-// }
+// const array = [1, 2, 3, 4, 5];
 
-// console.log(calcBMI('68,3', '1.65'));
-// console.log(calcBMI('118,3', '1.95'));
+// const newArray = array.concat()
+
+// const newArray = [...array];
+
+// console.log(newArray);
+
+// console.log(array === newArray);
+
+//* Об'єднання масивів через spread замість concat
+
+// const array1 = [1, 2, 3, 4];
+
+// const array2 = [5, 6, 7, 8];
+
+// const allElementsArray = array1.concat(array2);
+
+// const allElementsArray = [...array1, 60, ...array2, 40];
+
+// console.log(allElementsArray);
 
 /*
-  2. Деструктуризація
-
-  Перепиши функцію так, щоб вона приймала один об'єкт параметрів
-  замість набору незалежних аргументів
+  2. Копія об'єкту
 */
 
-// function printContactsInfo(names, phones) {
-//   const nameList = names.split(',');
-//   const phoneList = phones.split(',');
+// const user = {
+//   firstName: 'Jonathan',
+//   lastName: 'Barnett',
+//   age: 30,
+// };
 
-//   for (let i = 0; i < nameList.length, i < phoneList.length; i += 1) {
-//     console.log(`${nameList[i]}: ${phoneList[i]}`);
-//   }
-// }
+// const newUser = { ...user };
+
+// console.log(newUser);
+
+// console.log(newUser === user);
 
 /*
-  3. Глибока деструктуризація
+  3. Spread + Rest
 
-  Перепиши функцію так, щоб вона приймала один об'єкт параметрів
-  замість набору незалежних аргументів
+  Напишіть функцію getMinNumber яка приймає довільну кількість чисел
+  та повертає найменше із них
 */
 
-// function getBotReport(companyName, repairBots, defenceBots) {
-//   return `${companyName} has ${repairBots + defenceBots} bots in stock`;
+// function getMinNumber(...numbers) {
+//   // console.log(arguments);
+//   // console.log(number1, numbers);
+
+//   return Math.min(...numbers);
 // }
 
-// Було так:
-// console.log(getBotReport('Cyberdyne Systems', 150, 50))
+// console.log(getMinNumber(10, 50, 40, -5));
 
-// Має стати так:
-// console.log(getBotReport({
-//     companyName: 'Cyberdyne Systems',
-//     bots: {
-//         repairBots: 150,
-//         defenceBots: 50,
+/*
+  4. Деструктуризація об'єктів, які проблеми вирішує
+*/
+
+/*
+  Напишіть деструктуризуюче присвоювання, яке:
+  властивість firstName присвоїть у змінну firstName
+  властивість age присвої присвоїть у змінну userAge
+  властивість isAdmin присвоїть у змінну isAdmin (false, по дефолту
+  якщо, у об'єкта данної властивості не буде)
+*/
+
+// const user = {
+//   firstName: 'Andrii',
+//   lastName: 'Shevchuk',
+//   age: 30,
+//   hobbies: ['guitar', 'programming'],
+//   gender: 'male',
+//   weight: 70,
+//   heigth: 180,
+// };
+
+// const { firstName, lastName, age: userAge, hobbies = [], ...otherProps } = user;
+
+// console.log(userAge, hobbies);
+
+// console.log(otherProps);
+
+// user.firstName
+// user.lastName
+// user.age
+
+/*
+  5. Глубока деструктуризація об'єктів, дістати також original та secondary
+*/
+
+// const team = {
+//   number: 4,
+//   flag: './images/flag.jpg',
+//   employees: ['Anton', 'Oleg', 'Ronnie', 'Carr'],
+//   langs: {
+//     original: 'uk',
+//     secondary: 'en',
+//   },
+// };
+
+// const {
+//   flag,
+//   number,
+//   employees,
+//   langs: { original, secondary },
+// } = team;
+
+/*
+  6. Деструктуризація масивів
+*/
+
+/*
+  Дістати перший та третій елементи масиву, всі інші після 
+  третього занести в окремий масив
+*/
+
+// const names = [
+//   'Herbert Todd',
+//   'Belle Soto',
+//   'Roger Marsh',
+//   'Ethan Lindsey',
+//   'Olga Petrenko',
+// ];
+
+// const [first, , third, ...otherNames] = names;
+
+// console.log(first, third);
+
+// console.log(otherNames);
+
+/*
+  Дістати значення кольорів та властивість alpha
+*/
+
+// const rgb = [0, 255, 34, 0.5];
+
+// const [red, green, blue, alpha = 1] = rgb;
+
+// console.log(rgb);
+
+// console.log(...rgb);
+
+/*
+  У нас є об'єкт salaries з зарплатами
+
+  Створіть функцію topSalary(salaries), яка повертає ім'я найбільш
+  високооплачуваного працівника
+
+  Якщо об'єкт salaries пустий то повернути null
+
+  Якщо декілька людей з одною тою самою зарплатою є найбільш високоплачуваними, можна 
+  повернути будь-якого з них
+ 
+  Використовуйте Object.entries та деструктуризацію, щоб перебрати пари ключ/значение.
+*/
+
+// const salaries = {
+//   John: 100,
+//   Pete: 300,
+//   Mary: 250,
+// };
+
+// function topSalary(salaries) {
+//   const entries = Object.entries(salaries);
+
+//   // console.log(entries);
+
+//   let topSalary = 0;
+
+//   let topPaidEmployee = null;
+
+//   for (let [name, salary] of entries) {
+//     if (salary > topSalary) {
+//       topSalary = salary;
+//       topPaidEmployee = name;
 //     }
+//   }
 
-/*
-  4. Деструктуризація
-
-  Напиши функцію щоб вона приймала об'єкт параметрів із властивостями 
-  companyName і stock та виводила репорт про кількість товарів на 
-  складі будь-якої компанії.
-*/
-
-// console.log(
-//     getStockReport({
-//         companyName: 'Cyberdyne Systems',
-//         stock: {
-//             repairBots: 150,
-//             defenceBots: 50,
-//         },
-//     }),
-// ); // "Cyberdyne Systems has 200 items in stock"
-
-// console.log(
-//     getStockReport({
-//         companyName: 'Belacci',
-//         stock: {
-//             shoes: 20,
-//             skirts: 10,
-//             hats: 5,
-//         },
-//     }),
-// ); // "Belacci has 35 item in stock"
-
-/* 
-  5. Spread
-  Доповни функцію createContact(partialContact) так, щоб вона повертала 
-  новий об'єкт контакту з доданою властивістю id, а також list зі 
-  значенням "default" якщо у partialContact немає такої властивості.
-*/
-
-// function createContact(partialContact) {
+//   return topPaidEmployee;
 // }
 
-// function generateId() {
-//     return '_' + Math.random().toString(36).substr(2, 9);
+// console.log(topSalary(salaries));
+
+// function sayHi(name, age, heigth) {
+//   console.log(`Hi, ${name}! Your age is ${age}, your heigth is ${heigth}`);
 // }
 
-// console.log(
-//     createContact({
-//         name: 'Mango',
-//         email: 'mango@mail.com',
-//         list: 'friends',
-//     }),
-// );
-// console.log(
-//     createContact({
-//         name: 'Poly',
-//         email: 'poly@hotmail.com',
-//     }),
-// );
+// sayHi(30, 'Andrii', 180);
 
-/*
-  6. Rest
-  Напиши функцію transformUsername(user) так, щоб вона повертала новий 
-  об'єкт із властивістю fullName, замість firstName та lastName зі збережнням
-  усіх інших полів
-*/
+// function sayHi({ name, age, heigth } = {}) {
+//   console.log(`Hi, ${name}! Your age is ${age}, your heigth is ${heigth}`);
+// }
 
-// console.log(
-//     transformUsername({
-//         id: 1,
-//         firstName: 'Jacob',
-//         lastName: 'Mercer',
-//         email: 'j.mercer@mail.com',
-//         friendCount: 40,
-//     }),
-// );
+// function sayHi(user) {
+//   const { name, age, heigth } = user;
 
-// console.log(
-//     transformUsername({
-//         id: 2,
-//         firstName: 'Adrian',
-//         lastName: 'Cross',
-//         email: 'a.cross@hotmail.com',
-//         friendCount: 20,
-//     }),
-// );
+//   console.log(`Hi, ${name}! Your age is ${age}, your heigth is ${heigth}`);
+// }
+
+// sayHi();
+
+// const user = { name: `Andrii` };
+
+// const user2 = { ...user };
