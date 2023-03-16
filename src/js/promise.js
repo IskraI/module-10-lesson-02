2fc31f01ff82c09f4d1cb67162845d086b1fbe54
GoItTutor <sheva09a@gmail.com>
2023-03-16 20:36:17 +0100

    module-09-lesson-02

--- src/js/promise.js
+++ src/js/promise.js
@@ -6,9 +6,56 @@
 
 //? конструктор new Promise(callback(resolve, reject));
 
+// const promise = new Promise((resolve, reject) => {
+//   const isDone = Math.random();
+
+//   setTimeout(() => {
+//     if (isDone > 0.5) {
+//       console.log('Проміс виконався успішно');
+
+//       resolve('Done');
+//     }
+
+//     console.log('Проміс виконався неуспішно');
+//     reject('Error');
+//   }, 500);
+// });
+
+// console.log(promise);
+
+// const arr = [1, 2, 3];
+
+// console.log(arr);
+
+// arr.push(5);
+
 //? then(onSuccess, onError)
 
+// promise.then(data => {
+//   console.log(data);
+// });
+
 //? ланцюжок промісів та catch(onError)
+
+// promise.catch(error => {
+//   console.log(error);
+// });
+
+// promise
+//   .then(data1 => {
+//     console.log(data1);
+
+//     return data1 + '!';
+//   })
+//   .then(data2 => {
+//     console.log(data2);
+//   })
+//   .catch(error => {
+//     console.log(error);
+//   })
+//   .finally(() => {
+//     console.log('Finally!');
+//   });
 
 //? finally()
 
@@ -50,21 +97,21 @@
 //? TASK 03
 // Що буде у консолі
 
-// const promise = new Promise((resolve, reject) => {
-//   reject('error');
-// });
+const promise = new Promise((resolve, reject) => {
+  reject('error');
+});
 
-// promise
-//   .then(data => {
-//     console.log(data);
-//   })
-//   .then(data => {
-//     console.log(data);
-//     return '2';
-//   })
-//   .then(data => {
-//     console.log(data);
-//   })
-//   .catch(error => {
-//     console.log(error);
-//   });
+promise
+  .then(data => {
+    console.log(data);
+  })
+  .then(data => {
+    console.log(data);
+    return '2';
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.log(error);
+  });
--- src/js/promisification.js
+++ src/js/promisification.js
@@ -25,7 +25,7 @@
 //   .catch(console.log);
 
 //? Запит за користувачем на колбэках
-// const fetchUserByName = (name, onSuccess, onError) => {
+// const fetchUserByName = (name, resolve, onError) => {
 //   console.log('Робимо запит на сервер...');
 
 //   setTimeout(() => {
@@ -56,3 +56,82 @@
 // fetchUserByName('Ida', handleSuccessFetch, handleErrorFetch);
 
 //? Запит за користувачем на промісах
+
+// const fetchUserByName = name => {
+//   console.log('Робимо запит на сервер...');
+
+//   const promise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       const isDone = Math.random();
+
+//       if (isDone < 0.5) {
+//         const user = {
+//           firstName: name,
+//           lastName: 'Francis',
+//           age: 30,
+//         };
+
+//         resolve(user);
+//       } else {
+//         reject("Користувача з таким ім'ям не знайдено");
+//       }
+//     }, 2000);
+//   });
+
+//   return promise;
+// };
+
+// fetchUserByName(`Andrii`).then(console.log).catch(console.log);
+
+// const isSuccess = Math.random() > 0.5;
+
+// const promise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     if (isSuccess) {
+//       resolve('Success! Value passed to resolve function');
+//     } else {
+//       reject('Error! Error passed to reject function');
+//     }
+//   }, 500);
+// });
+
+// promise
+//   .then(value => {
+//     console.log('onResolve call inside promise.then()');
+//     console.log(value);
+//   })
+//   .catch(console.log);
+
+//   i - 0
+
+// 1500
+
+// 500
+
+//   firstDelay + delayStep * i
+
+// const arr = [1, 2, 3];
+
+// const consoleLog = (el, index) => {
+//   console.log(el, index);
+// };
+
+// arr.forEach(el => {
+//   console.log(el);
+// });
+
+const forEach = (callback, array) => {
+  for (let index = 0; index < array.length; index += 1) {
+    callback(array[index], index, array);
+  }
+};
+
+const map = (callback, array) => {
+  const resultArr = [];
+
+  for (let index = 0; index < array.length; index += 1) {
+    resultArr.push(callback(array[index], index, array));
+  }
+
+  return resultArr;
+};
