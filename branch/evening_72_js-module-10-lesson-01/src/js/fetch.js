e2f39a2360dec1e5fc88e768a375d66ce38071dc
Irina Iskra <iskrairina.a@gmail.com>
2023-03-23 23:12:35 +0200

    branch

---
+++ branch/evening_72_js-module-10-lesson-01/src/js/fetch.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// https://habr.com/ru/post/495698/
+
+// https://jsonplaceholder.typicode.com/
+
+// fetch('https://jsonplaceholder.typicode.com/users?_limit=5&_sort=username')
+//   .then(response => {
+//     if (!response.ok) {
+//       throw new Error(response.status);
+//     }
+
+//     return response.json();
+//   })
+//   .then(data => {
+//     console.log(data);
+//   })
+//   .catch(err => {
+//     console.warn(err);
+//   });
